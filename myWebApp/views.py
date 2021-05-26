7bcb298343a6bc82af6dbe7dc35749fede6d06ff
Akhilesh Hessa <akhilesh4us@gmail.com>
2021-05-26 18:51:57 +0530

    problems

--- myWebApp/views.py
+++ myWebApp/views.py
@@ -1,3 +1,28 @@
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import date, datetime
+import json
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Hello World!')
+
+@api_view(["POST"])
+def computeAge(dd, mm, yyyy): 
+
+    day = json.loads(dd.body)
+    month = json.loads(mm.body)
+    year = json.loads(yyyy.body)
+
+    try:
+        bday = datetime.date(year, month, day)
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
+    today = date.today()
+
+    return JsonResponse("Birthday: "+bday+"\nToday: "+today, safe=False)
+    # return JsonResponse("Today: "+today.strftime('%d-%m-%Y'), safe=False)
